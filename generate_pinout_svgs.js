c2334ee59bf57463a3a850d6b5629b5cb70f55a1
Juha-Jarmo Heinonen <o@sorsacode.com>
2017-10-19 06:42:05 +0300

    Added MOLEX Micro-Fit shapes and defined the pinouts for DISTRAP v0.7

--- generate_pinout_svgs.js
+++ generate_pinout_svgs.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const title = 'DISTRAP Bus Connector v0.6';
+const title = 'DISTRAP Bus Connector v0.7';
 
 const sigDefaults = {
   bgColor: '#ffffff',
@@ -13,14 +13,23 @@ const signals = {
     bgColor: '#cccccc',
     notes: 'Not Connected'
   },
-  '5VDC': {
+  'CAN PWR': {
     textColor: '#ffffff',
     bgColor: '#ff3344',
-    notes: 'Max 250mA per device'
+    notes: 'CAN bus power; +5VDC; max 250mA'
+  },
+  'CAN PWR (optional)': {
+    textColor: '#cccccc',
+    bgColor: '#ff3344',
+    notes: 'CAN bus power: +5V DC; max 250mA; optional'
   },
   '12VDC': {
     bgColor: '#fdff41',
-    notes: '5A per pin max'
+    notes: '+12V DC power; max 5A per pin'
+  },
+  '24VDC': {
+    bgColor: '#6d77ff',
+    notes: '+24V DC power; max 5A per pin'
   },
   'GND': {
     textColor: '#ffffff',
@@ -29,144 +38,64 @@ const signals = {
   },
   'CAN LO': {
     bgColor: '#c0ffbf',
-    notes: 'CANbus LO signal'
+    notes: 'CAN bus Low signal'
   },
   'CAN HI': {
     bgColor: '#bfffff',
-    notes: 'CANbus HI signal'
+    notes: 'CAN bus High signal'
   },
   'CAN GND': {
-    bgColor: '#003366',
+    bgColor: '#1a4b19',
     textColor: '#ffffff',
-    notes: 'CANbus GND'
+    notes: 'CAN bus GND'
   },
 };
 
 const pinouts = {
   '2x2': {
-    notes: '4-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); no power rail',
+    name: 'Logic',
+    notes: '4-pin dual-row MOLEX Micro-Fit (3.0mm pitch); no power rail',
     pinout: [
       ['CAN HI', 'CAN LO'],
-      ['N/C', 'CAN GND'],
+      ['CAN PWR (optional)', 'CAN GND'],
     ]
   },
   '2x3': {
-    notes: '6-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 5A power rail',
+    name: 'Power and Logic 24V',
+    notes: '6-pin dual-row MOLEX Micro-Fit (3.0mm pitch); 24V 5A (120W) power rail',
     pinout: [
       ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
+      ['CAN PWR', 'CAN GND'],
+      ['24VDC', 'GND'],
     ]
   },
   '2x4': {
-    notes: '8-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 10A power rail',
+    name: 'Power and Logic 12V',
+    notes: '8-pin dual-row MOLEX Micro-Fit (3.0mm pitch); 12V 10A (120W) power rail',
     pinout: [
       ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
+      ['CAN PWR', 'CAN GND'],
       ['12VDC', 'GND'],
       ['12VDC', 'GND'],
     ]
   },
   '2x5': {
-    notes: '10-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 15A power rail',
+    name: 'High Power and Logic 24V',
+    notes: '10-pin dual-row MOLEX Micro-Fit (3.0mm pitch); 24V 15A (360W) power rail',
     pinout: [
       ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-    ]
-  },
-  '2x6': {
-    notes: '12-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 20A power rail',
-    pinout: [
-      ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-    ]
-  },
-  '2x7': {
-    notes: '14-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 25A power rail',
-    pinout: [
-      ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
+      ['CAN PWR', 'CAN GND'],
+      ['24VDC', 'GND'],
+      ['24VDC', 'GND'],
+      ['24VDC', 'GND'],
     ]
   },
   '2x8': {
-    notes: '16-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 30A power rail',
+    name: 'High Power and Logic 12V',
+    notes: '16-pin dual-row MOLEX Micro-Fit (3.0mm pitch); 12V 30A (360W) power rail',
     pinout: [
       ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-    ]
-  },
-  '2x9': {
-    notes: '18-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 35A power rail',
-    pinout: [
-      ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-    ]
-  },
-  '2x10': {
-    notes: '20-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 40A power rail',
-    pinout: [
-      ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-    ]
-  },
-  '2x11': {
-    notes: '22-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 45A power rail',
-    pinout: [
-      ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-    ]
-  },
-  '2x12': {
-    notes: '24-pin dual-row MOLEX Mini-Fit Jr (4.2mm pitch); 50A power rail',
-    pinout: [
-      ['CAN HI', 'CAN LO'],
-      ['5VDC', 'CAN GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
-      ['12VDC', 'GND'],
+      ['CAN PWR', 'CAN GND'],
       ['12VDC', 'GND'],
       ['12VDC', 'GND'],
       ['12VDC', 'GND'],
@@ -177,21 +106,45 @@ const pinouts = {
   },
 };
 
-// 1 = notched, 0 = square
-const minifitKeys = [
-  0, 1, // 2x1
-  1, 0, // 2x2
-  1, 0, // 2x3
-  0, 1, // 2x4
-  0, 1, // 2x5
-  1, 0, // 2x6
-  1, 0, // 2x7
-  0, 1, // 2x8
-  0, 1, // 2x9
-  1, 0, // 2x10
-  1, 0, // 2x11
-  0, 1, // 2x12
-];
+// key pins:
+// 0:   █ square
+// 2:   ▜ left
+// 3:   ▛ right
+
+const microfitKeys = {
+  '2x2': [
+    3, 2, // 2
+    2, 3  // 4
+  ],
+  '2x3': [
+    2, 2, // 2
+    2, 2, // 4
+    2, 3  // 6
+  ],
+  '2x4': [
+    3, 0, // 2
+    2, 2, // 4
+    3, 3, // 6
+    2, 2  // 8
+  ],
+  '2x5': [
+    2, 2, // 2
+    3, 3, // 4
+    2, 3, // 6
+    2, 3, // 8
+    0, 3  // 10
+  ],
+  '2x8': [
+    3, 3, // 2
+    3, 2, // 4
+    0, 3, // 6
+    3, 3, // 8
+    2, 2, // 10
+    2, 3, // 12
+    3, 3, // 14
+    3, 3  // 16
+  ]
+};
 const connectorShapes = {
   receptacle: ({x, y, colCount, gridW, gridH, id}) => `
 <path d="M${x - 6},${y - 6}
@@ -211,7 +164,7 @@ const connectorShapes = {
       L${x + colCount / 2 * gridW - gridW / 2 + 13},${y - 6}"
       fill="#ffffff" stroke="#666666" stroke-width="3" />
 <text x="${x}" y="${y + gridH * 2 + 26}">
-  <tspan font-family="HelveticaNeue" font-size="18" fill="#000000">Receptacle, front view</tspan>
+  <tspan font-family="HelveticaNeue" font-size="18" fill="#000000">Receptacle, mating side (Plug/header, wire/PCB side)</tspan>
 </text>`,
   plug: ({x, y, colCount, gridW, gridH, id}) => `
 <path d="M${x - 6},${y - 6}
@@ -224,19 +177,19 @@ const connectorShapes = {
       L${x - 6},${gridH * 2 + y + 3} L${x - 6},${y - 6} z"
       fill="#eeeeee" stroke="#666666" stroke-width="3" />
 <text x="${x}" y="${y + gridH * 2 + 26}">
-  <tspan font-family="HelveticaNeue" font-size="18" fill="#000000">Plug/header, front view</tspan>
+  <tspan font-family="HelveticaNeue" font-size="18" fill="#000000">Plug/header, mating side (Receptacle, wire side)</tspan>
 </text>`,
 };
 const keyShapes = [
-  // squared:
+  // squared (0):
   (x, y, {textColor, bgColor, pinNumber}) => `
 <path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 58} L${x},${y + 58} L${x},${y} z" fill="${bgColor}"/>
 <path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 58} L${x},${y + 58} L${x},${y} z" fill-opacity="0"
       stroke="#333333" stroke-width="1"/>
-<text x="${x + 29}" y="${y + 29}" text-anchor="middle" dominant-baseline="central">
+<text x="${x + 29}" y="${y + 29}" text-anchor="middle" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle">
   <tspan font-family="HelveticaNeue-Bold" font-size="24" fill="${textColor}">${pinNumber}</tspan>
 </text>`,
-  // notched
+  // notched (1; mini-fit):
   (x, y, {textColor, bgColor, pinNumber}) => `
 <path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 41} L${x + 40},${y + 58} L${x + 17},${y + 58}
       L${x},${y + 41} L${x},${y} z"
@@ -244,20 +197,41 @@ const keyShapes = [
 <path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 41} L${x + 40},${y + 58} L${x + 17},${y + 58}
       L${x},${y + 41} L${x},${y} z"
       fill-opacity="0" stroke="#333333" stroke-width="1"/>
-<text x="${x + 29}" y="${y + 29}" text-anchor="middle" dominant-baseline="central">
+<text x="${x + 29}" y="${y + 29}" text-anchor="middle" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle">
+  <tspan font-family="HelveticaNeue-Bold" font-size="24" fill="${textColor}">${pinNumber}</tspan>
+</text>`,
+  // left (2; micro-fit):
+  (x, y, {textColor, bgColor, pinNumber}) => `
+<path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 41} L${x + 40},${y + 58} L${x},${y + 58} L${x},${y} z"
+      fill="${bgColor}"/>
+<path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 41} L${x + 40},${y + 58} L${x},${y + 58} L${x},${y} z"
+      fill-opacity="0" stroke="#333333" stroke-width="1"/>
+<text x="${x + 29}" y="${y + 29}" text-anchor="middle" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle">
+  <tspan font-family="HelveticaNeue-Bold" font-size="24" fill="${textColor}">${pinNumber}</tspan>
+</text>`,
+  // right (3; micro-fit):
+  (x, y, {textColor, bgColor, pinNumber}) => `
+<path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 58} L${x + 17},${y + 58} L${x},${y + 41} L${x},${y} z"
+      fill="${bgColor}"/>
+<path d="M${x},${y} L${x + 58},${y} L${x + 58},${y + 58} L${x + 17},${y + 58} L${x},${y + 41} L${x},${y} z"
+      fill-opacity="0" stroke="#333333" stroke-width="1"/>
+<text x="${x + 29}" y="${y + 29}" text-anchor="middle" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle">
   <tspan font-family="HelveticaNeue-Bold" font-size="24" fill="${textColor}">${pinNumber}</tspan>
 </text>`,
 ];
 
-const svgTemplate = ({width, height, id, notes, connectors}) => `<?xml version="1.0" encoding="UTF-8"?>
+const svgTemplate = ({width, height, id, name, notes, connectors}) => `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
      x="0" y="0" width="${width}" height="${height}" viewBox="0, 0, ${width}, ${height}">
-<text x="20" y="42">
-  <tspan font-family="HelveticaNeue-Bold" font-size="36" fill="#000000">${title}: ${id}</tspan>
+<text x="20" y="32">
+  <tspan font-family="HelveticaNeue" font-size="24" fill="#000000">${title}</tspan>
 </text>
-<text x="20" y="72">
-  <tspan font-family="HelveticaNeue-Bold" font-size="18" fill="#000000">${notes}</tspan>
+<text x="20" y="62">
+  <tspan font-family="HelveticaNeue-Bold" font-size="24" fill="#000000">${name}</tspan>
+</text>
+<text x="20" y="92">
+  <tspan font-family="HelveticaNeue" font-size="18" fill="#000000">${notes}</tspan>
 </text>
 ${connectors}
 <text x="20" y="520">
@@ -268,13 +242,13 @@ ${connectors}
 
 const pinoutRowTemplate = (y, {pinNumber, label, notes, bgColor, textColor}) => `
 <circle cx="30" cy="${y - 12}" r="16" fill="${bgColor}" stroke="#333333" stroke-width="2" />
-<text x="30" y="${y - 12}" text-anchor="middle" dominant-baseline="central">
+<text x="30" y="${y - 12}" text-anchor="middle" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle">
   <tspan font-family="HelveticaNeue-Bold" font-size="22" fill="${textColor}">${pinNumber}</tspan>
 </text>
-<text x="54" y="${y - 12}" dominant-baseline="central" text-anchor="left">
+<text x="54" y="${y - 12}" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle" text-anchor="left">
   <tspan font-family="HelveticaNeue-Bold" font-size="18" fill="#000000">${label}</tspan>
 </text>
-<text x="140" y="${y - 12}" dominant-baseline="central" text-anchor="left">
+<text x="240" y="${y - 12}" dominant-baseline="middle" dy="0.5em" alignment-baseline="middle" text-anchor="left">
   <tspan font-family="HelveticaNeue" font-size="18" fill="#000000">${notes}</tspan>
 </text>
 `;
@@ -288,6 +262,7 @@ const mkSVG = id => {
   let y = 130 + gridH;
   let isLower = true;
   const pinData = pinouts[id];
+  const name = pinData.name;
   const notes = pinData.notes;
   const cols = pinData.pinout;
   const colCount = cols.length;
@@ -305,10 +280,10 @@ const mkSVG = id => {
         pinNumber,
         label: sigId,
         notes: sig.notes ? sig.notes : sigDefaults.notes,
-        minifitKey: minifitKeys[pinIndex]
+        microfitKey: microfitKeys[id][pinIndex]
       };
       pinLegend[pinNumber] = pinParam;
-      pinoutSVGarr.push(keyShapes[pinParam.minifitKey](x, y, pinParam));
+      pinoutSVGarr.push(keyShapes[pinParam.microfitKey](x, y, pinParam));
       if (!isLower) {
         colNum++;
         x += gridW;
@@ -335,7 +310,14 @@ const mkSVG = id => {
       const pinParam = pinLegend[pinNumber];
       pinParam.x = x;
       pinParam.y = y;
-      pinoutSVGarr.push(keyShapes[pinParam.minifitKey](x, y, pinParam));
+      let keyId = pinParam.microfitKey;
+      if (keyId === 2) {
+        keyId = 3;
+      }
+      else if (keyId === 3) {
+        keyId = 2;
+      }
+      pinoutSVGarr.push(keyShapes[keyId](x, y, pinParam));
       if (!isLower) {
         colNum++;
         x -= gridW;
@@ -354,7 +336,7 @@ const mkSVG = id => {
     y += 36;
   });
   return svgTemplate({
-    id, notes,
+    id, name, notes,
     width: 800,
     height: y + 2 * gridH,
     connectors: pinoutSVGarr.join('\n')
